52134fa83bf82cd3e28e48e1632b53cd641eb359
ArthurZhao <jzha7358@uni.sydney.edu.au>
2023-02-06 11:14:34 +0800

    make html

---
+++ source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+        '.md': CommonMarkParser,
+        }
+source_suffix = ['.rst', '.md']
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+project = 'MindSpore-Problem-Solving-Book'
+copyright = '2023, JichenZhao'
+author = 'JichenZhao'
+release = '0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. MindSpore-Problem-Solving-Book documentation master file, created by
+   sphinx-quickstart on Mon Feb  6 11:09:34 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to MindSpore-Problem-Solving-Book's documentation!
+==========================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
